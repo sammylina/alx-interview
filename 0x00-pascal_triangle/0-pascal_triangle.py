32fb5d9b6f7d990d63f88f25d1ab76a5b5604c97
SammyAlx <samuelhailu49@gmail.com>
2023-06-28 15:03:20 +0300

    print Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+
+    main_arr = []
+    for row in range(1, n + 1):
+        arr = []
+        for col in range(1, row + 1):
+            if col == 1 or col == row:
+                arr.append(1)
+            elif col < row:
+                val = main_arr[row - 2][col - 1] + main_arr[row - 2][col - 2]
+                arr.append(val)
+        main_arr.append(arr)
+    return main_arr
