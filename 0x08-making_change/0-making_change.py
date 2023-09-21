cbe17944c895e16413d65de99486f4497ffa5bd1
sammyALX <samuelhailu49@gmail.com>
2023-09-21 23:39:10 +0300

    add annotation to arguments, variables and function definition

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,19 +3,19 @@
 """
 
 
-def makeChange(coins, total):
+def makeChange(coins: list, total: int) -> int:
     """ Given a pile of coins , determine the fewest
         number of coins needed to make total
     """
-    if len(coins) == 0 or total == 0:
+    if len(coins) == 0 or total <= 0:
         return -1
-    coins = sorted(coins)
-    remaining_total = total
-    coins_count = 0
+    coins: list = sorted(coins)
+    remaining_total: int = total
+    coins_count: int = 0
     while remaining_total:
         if len(coins) == 0:
             return -1
-        coin = coins.pop()
+        coin: int = coins.pop()
         if remaining_total < coin:
             return -1
         coins_count += remaining_total // coin
